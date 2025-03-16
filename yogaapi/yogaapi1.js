7b18965dc5bd593e460b1154df6e07a98c84c579
Julie Hart <147504798+JulieH-Oct23@users.noreply.github.com>
2025-03-15 21:02:53 -0600

    updating for publishing

--- yogaapi/yogaapi1.js
+++ yogaapi/yogaapi1.js
@@ -1,4 +1,23 @@
-fetch('https://github.com/alexcumplido/yoga-api/v1/categories')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.log(error));
+
+  const yogaCount = 48;
+  let yogaDex = {};
+
+window.onload = async function( ) {
+    getPose(1);
+}
+
+  async function getPose(num) {
+        let url='https://yoga-api-nzy4.onrender.com/v1/poses' + num.toString( );
+        
+        let res = await fetch(url);
+        let pose = await res.json( );
+        console.log(pose);
+
+        let poseEnglishName = pose['english_name'];
+        let poseSanskritName = pose['sanskrit_name'];
+        let poseBenefits = benefits['pose_benefits'];
+        let poseCue = cue['pose_description'];
+        let poseVIdeo = video['url_video'];
+
+  }
+
