992ee25e1bf94a91ddb05c68a6f19b9cd7682d3d
Julie Hart <147504798+JulieH-Oct23@users.noreply.github.com>
2025-03-17 14:17:50 -0600

    updating yogaapi after call with ron

--- yogaapi/yogaapi1.js
+++ yogaapi/yogaapi1.js
@@ -1,29 +1,62 @@
 
-  const yogaCount = 48;
+  const yogaCount = 1;
+  let poses  = null;
   let yogaDex = {};
 
 window.onload = async function( ) {
-    getYoga(1);
+      poses = await getPoses()
+      //console.log(poses)
+
+      const poseSel = document.getElementById('indvPose')
+      poses.forEach(pose => {
+        let option = document.createElement('option')
+        option.value = pose.id
+        option.text = pose['english_name']
+        poseSel.appendChild(option)
+      });
+};
+
+function test(event) {
+  setPoseName(event.target.value)
 }
 
-  async function getYoga(num) {
-        let url = "https://yoga-api-nzy4.onrender.com/v1/poses/"
+function setPoseName(id) {
+  console.log(poses)
+  const pose = poses.find(p => p.id === parseInt(id))
+  const poseEnglishNameLabel = document.getElementById('english_pose_name')
+  poseEnglishNameLabel.innerHTML = pose['english_name']
+  console.log(pose)
+}
+
+async function getPoses() {
+  let url = "https://yoga-api-nzy4.onrender.com/v1";
+        let res = await fetch(url);
+        let yogaApi = await res.json( );
+        
+        let poseRes = await fetch(yogaApi.poses);
+        poses = await poseRes.json( );
+        return poses
+}
+
+  async function getYoga() {
+        let url = "https://yoga-api-nzy4.onrender.com/v1";
         
         let res = await fetch(url);
-        let yoga = await res.json( );
-        console.log(yoga);
+        let yogaApi = await res.json( );
+        console.log(yogaApi)
+    
+        // let englishName = yogaApi['english_name'];
+        // let sanskritName = yogaApi['sanskrit_name'];
+        // let poseBenefits = yogaApi['pose_benefits'];
+        // let poseDescription = yogaApi['pose_description'];
+        // let translationName = yogaApi['translation_name'];
+        
+        
+        let poseRes = await fetch(yogaApi.poses);
+        poses = await poseRes.json( );
+        console.log(poses);
 
-        let yogaName = yoga['english_name'];
-        let sanskritName = yoga['sanskrit_name'];
-        let yogaBenefits = yoga['pose_benefits'];
-        let yogaCue = yoga['pose_description'];
-        let yogaMeaning = yoga['translation_name'];
-        // let yogaImg = yoga['url_png']['url_svg']['url_svg_alt'];
-
-        res = await fetch(yoga['english_name']);
-        let yogaDesc = await res.json();
-
-        yogaDex[1] = {"english_name": yogaName, 'sanskrit_name': sanskritName, "pose_benefits": yogaBenefits, "pose_description": yogaCue, "translation_name": yogaMeaning}
-
-     
+        // yogaDex[num] = { "english_name": englishName, 'sanskrit_name': sanskritName, "pose_benefits": poseBenefits, "pose_description": poseDescription, "translation_name": translationName}
   }
+
+  console.log(poses)
