f5d0103439bbbc18dbd866dc488542a2acad7b29
Julie Hart <147504798+JulieH-Oct23@users.noreply.github.com>
2025-04-16 15:13:17 -0600

    done

--- assignment129restspread.js
+++ assignment129restspread.js
@@ -1,54 +1,138 @@
-function animalSightings(...animals) {
-    console.log(`Sitings: ${animals}`);
+// All 12.9 assignments
+
+// Exercise 1 Named Exports and Imports
+// inventory.mjs
+const items = [];
+
+export const addItem = (item) =>
+{
+	items.push(item);
+	console.log(`Item added: ${item}`);
+};
+export const removeItem = (item) =>
+{
+	let isRemoved = false;
+
+	for (let i = 0; i < items.length; i++)
+	{
+		if (items[i] === item)
+		{
+			isRemoved = true;
+			items.splice(i, 1);
+			i--;
+		}
+	}
+
+	if (isRemoved)
+	{
+		console.log(`Item removed: ${item}`);
+	}
+	else
+	{
+		console.log(`Item not exist: ${item}`);
+	}
+};
+export const listItems = () =>
+{
+	console.log(`Listing all items:`);
+
+	for (const item of items)
+	{
+		console.log(`* ${item}`);
+	}
+};
+
+
+
+// storemanager.mjs
+import {addItem, removeItem, listItems} from "./inventory.mjs";
+
+addItem("Book");
+addItem("Pen");
+addItem("Pencil");
+listItems();
+
+removeItem("Pen");
+listItems();
+
+// Exercise 2 - Default Export and Import
+
+// post.mjs
+export default class Post
+{
+	constructor (title, content)
+	{
+		this.title = title;
+		this.content = content;
+	}
+
+	publish ()
+	{
+		console.log(`Publishing post: ${this.title}`);
+	}
 }
 
-animalSightings("Yogi the Yorkie", "Booberry the Bernard", "Larry the Dad", "Harry the Teen", "Rhino", "Lion", "Starfish");
+// blog.mjs
+import Post from "./Post.mjs";
+
+const myPost = new Post("Hello World", "This is my first post.");
+myPost.publish();
 
 
-const forestHabitats = ["Forest A", "Forest B"];
-const savannahHabitats = ["Savannah C", "Savannah D"]
-const combinedHabitats= [...forestHabitats, ...savannahHabitats];
-console.log(`Combined Habitats: ${combinedHabitats}`);
+// Exercise 3 - Re-exporting Modules
+// app.mjs
+import {capitalize, square} from "./utils/index.mjs";
 
+console.log(capitalize("hello"));
+console.log(square(4));
 
-const starFishStatus = {
-    population: 5,
-    status: "Endangered"
+// index.mjs
+export * from './stringUtils.mjs';
+export * from './numberUtils.mjs';
+
+// numberUtils.mjs
+export const square = (n) => n * n;
+
+// stringUtils.mjs
+export const capitalize = (s) => s.charAt(0).toUpperCase() + s.slice(1);
+
+// Exercise 4 - Dynamic Imports
+// appTwo.js
+async function loadConfig ()
+{
+	const themeModule = await import("./theme.mjs");
+
+	const currentHour = new Date().getHours();
+
+	if (currentHour < 18)
+	{
+		themeModule.setLightTheme();
+	}
+	else
+	{
+		themeModule.setDarkTheme();
+	}
+}
+loadConfig();
+
+// theme.mjs
+let theme = null;
+
+export const setLightTheme = () =>
+{
+	console.log("Setting light theme.");
+	theme = "light";
 };
 
-const updatedStarFishStatus = {
-    ...starFishStatus, 
-    population:7,
-    status: "Needs Help"
+export const setDarkTheme = () =>
+{
+	console.log("Setting dark theme.");
+	theme = "dark";
 };
 
-console.log(`Updated Star Fish Status: ${updatedStarFishStatus}`);
+// Exercise 5
+// appThree.mjs
+import './globalConfig.mjs';
 
-
-const lionFishProfile = {
-    name: "Leo",
-    age: 3,
-    species: "Poison"
-};
-
-const lionFishGeneticProfile = {
-    ...lionFishProfile,
-    genetics: "Poison Spines"
-};
-
-console.log(`Original Lion Fish Profile: ${lionFishProfile}`);
-console.log(`Lion Fish Genetic Profile: ${lionFishGeneticProfile}`);
-
-
-const habitatHealth = {
-    waterQuality: "Excellent",
-    foodSupply:  {
-        herbivores: "Limited",
-        carnivores: "Abundant"
-    }    
-};
-
-const updatedHabitatHealth = {
-    ...habitatHealth, foodSupply: {...habitatHealth.foodSupply, herbibores:"Plentiful"}};
-console.log("Original Habitat Health:", habitatHealth);
-console.log("Updated Habitat Health:", updatedHabitatHealth);
+// globalConfig.mjs
+console.log("Welcome to the application! Initializing...");
