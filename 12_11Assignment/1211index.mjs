664e7a99676f6f933759cad624df50b0a34120dd
Julie Hart <147504798+JulieH-Oct23@users.noreply.github.com>
2025-04-21 09:33:48 -0600

    new

---
+++ 12_11Assignment/1211index.mjs
@@ -0,0 +1,3 @@
+export * from './stringUtils.mjs';
+export * from './numberUtils.mjs';
+
---
+++ 12_11Assignment/1211theme.mjs
@@ -0,0 +1,16 @@
+let theme = null;
+
+export const setLightTheme = () =>
+{
+	console.log("Setting light theme.");
+	theme = "light";
+};
+
+export const setDarkTheme = () =>
+{
+	console.log("Setting dark theme.");
+	theme = "dark";
+};
+
+
+import './globalConfig.mjs';
